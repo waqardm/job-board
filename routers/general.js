d919f5dd425ed9d1cc72d4026c481831fa8f9361
Waqar Mohammad <waqar@waqar.co>
2018-12-12 22:13:03 +0000

    Completed about, community, contact, login pages and skeleton for register page along with relevant routes

--- routers/general.js
+++ routers/general.js
@@ -4,5 +4,10 @@ const router = express.Router();
 const generalController = require('../controllers/general');
 
 router.get('/', generalController.getIndex);
+router.get('/about', generalController.getAbout);
+router.get('/community', generalController.getCommunity);
+router.get('/contact', generalController.getContact);
+router.get('/register', generalController.getRegister);
+router.get('/login', generalController.getLogin);
 
 module.exports = router;
