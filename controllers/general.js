000563d972591dfa3c0a55430ab6b03f85394570
Waqar Mohammad <waqar@waqar.co>
2018-12-13 10:39:19 +0000

    Updated to add some emojis, added directories for candidate && hirer along with relevant sign up pages for each. 404 && 500 pages also completed.

--- controllers/general.js
+++ controllers/general.js
@@ -28,19 +28,36 @@ module.exports.getContact = (req, res) => {
   });
 }
 
-//Renders Register Page
-module.exports.getLogin = (req, res) => {
-  res.render('login', {
-    pageTitle: 'Login | Job Board'
+//Renders Candidate Login Page
+module.exports.getCandidateLogin = (req, res) => {
+  res.render('candidate/login', {
+    pageTitle: 'Candidate Login | Job Board'
   });
 }
 
-//Renders Register Page
-module.exports.getRegister = (req, res) => {
-  res.render('register', {
-    pageTitle: 'Register | Job Board'
+//Renders Hirer Login Page
+module.exports.getHirerLogin = (req, res) => {
+  res.render('hirer/login', {
+    pageTitle: 'Hirer Login | Job Board'
+  });
+}
+
+//Renders Candidate Register Page
+module.exports.getCandidateRegister = (req, res) => {
+  res.render('candidate/register', {
+    pageTitle: 'Candidate Register | Job Board'
   })
 }
+
+//Renders Hirer Register Page
+module.exports.getHirerRegister = (req, res) => {
+  res.render('hirer/register', {
+    pageTitle: 'Hirer Register | Job Board'
+  })
+}
+
+
+
 
 
 
--- routers/general.js
+++ routers/general.js
@@ -7,7 +7,9 @@ router.get('/', generalController.getIndex);
 router.get('/about', generalController.getAbout);
 router.get('/community', generalController.getCommunity);
 router.get('/contact', generalController.getContact);
-router.get('/register', generalController.getRegister);
-router.get('/login', generalController.getLogin);
+router.get('/candidate/register', generalController.getCandidateRegister);
+router.get('/hirer/register', generalController.getHirerRegister);
+router.get('/candidate/login', generalController.getCandidateLogin);
+router.get('/hirer/login', generalController.getHirerLogin);
 
 module.exports = router;
