130d36abc100e0ce83848ec4d469b0c195c5a0bb
Irshad Ansari <irshadjsr21@gmail.com>
2018-12-13 10:53:39 +0530

    Merged With waqar

--- controllers/general.js
+++ controllers/general.js
@@ -1,19 +1,15 @@
 
 // Renders Index Page
 module.exports.getIndex = (req, res) => {
-<<<<<<< HEAD
-    res.render('index');
-}
-=======
     res.render("index", {
-      title: 'Get Your First Job in Tech | Job Board'
+      pageTitle: 'Get Your First Job in Tech | Job Board'
     });
 }
 
 // Renders About Page
 module.exports.getAbout = (req, res) => {
   res.render("about", {
-    title: "About Us | Job Board"
+    pageTitle: "About Us | Job Board"
   });
 };
 
@@ -21,31 +17,30 @@ module.exports.getAbout = (req, res) => {
 
 module.exports.getCommunity = (req, res) => {
   res.render('community', {
-    title: "Community | Job Board"
+    pageTitle: "Community | Job Board"
   });
 }
 
 // Renders Contact Page
 module.exports.getContact = (req, res) => {
   res.render("contact", {
-    title: 'Contact Us | Job Board'
+    pageTitle: 'Contact Us | Job Board'
   });
 }
 
 //Renders Register Page
 module.exports.getLogin = (req, res) => {
   res.render('login', {
-    title: 'Login | Job Board'
+    pageTitle: 'Login | Job Board'
   });
 }
 
 //Renders Register Page
 module.exports.getRegister = (req, res) => {
   res.render('register', {
-    title: 'Register | Job Board'
+    pageTitle: 'Register | Job Board'
   })
 }
 
 
 
->>>>>>> waqar
--- routers/general.js
+++ routers/general.js
@@ -4,13 +4,10 @@ const router = express.Router();
 const generalController = require('../controllers/general');
 
 router.get('/', generalController.getIndex);
-<<<<<<< HEAD
-=======
 router.get('/about', generalController.getAbout);
 router.get('/community', generalController.getCommunity);
 router.get('/contact', generalController.getContact);
 router.get('/register', generalController.getRegister);
 router.get('/login', generalController.getLogin);
->>>>>>> waqar
 
 module.exports = router;
