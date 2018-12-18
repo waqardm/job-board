78f8d66b38213ec014797b1a8427e300cb1a03ed
Waqar Mohammad <waqar@waqar.co>
2018-12-18 22:57:18 +0000

    javascript for dashboard on hirer && candidate. Worked on showing/hiding content on tab clicks

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,54 @@ function submitLogout(btn) {
     const form = btn.parentNode;
     form.submit();
 }
+
+
+// //applications && jobList tab --> Hirer
+// const applicationsTab = document.getElementById('applications-tab');
+// const jobsTab = document.getElementById('jobs-listed-tab');
+// applicationsTab.addEventListener('click', applications);
+// jobsTab.addEventListener('click', jobs);
+
+// const applicationsDiv = document.getElementById('applications');
+// const jobsDiv = document.getElementById('jobs-listed');
+//jobsDiv.style.display = "none";
+
+// //divs
+// function applications() {
+//     applicationsTab.classList.add('is-active');
+//     jobsTab.classList.remove(is-active');
+//     applicationsDiv.style.display = 'block';
+//     jobsDiv.style.display = 'none';
+// }
+
+// function jobs() {
+//     jobsTab.classList.add('is-active');
+//     jobsDiv.style.display = 'block';
+//     applicationsTab.classList.remove('is-active');
+//     applicationsDiv.style.display = 'none';
+// }
+
+//saved && applied tabs --> candidate
+const savedTab = document.getElementById('saved-tab');
+const appliedTab = document.getElementById('applied-tab');
+savedTab.addEventListener('click', savedJobs);
+appliedTab.addEventListener('click', appliedJobs);
+
+const savedDiv = document.getElementById('saved');
+const appliedDiv = document.getElementById('applied');
+appliedDiv.style.display = 'none';
+
+function savedJobs() {
+    savedTab.classList.add('is-active');
+    appliedTab.classList.remove('is-active');
+    savedDiv.style.display = 'block';
+    appliedDiv.style.display = 'none';
+}
+
+function appliedJobs() {
+    appliedTab.classList.add('is-active');
+    savedTab.classList.remove('is-active');
+    appliedDiv.style.display = 'block';
+    savedDiv.style.display = 'none';
+}
+
